f71ce6665a475ddbeaf89a89e20aae77cc7bdf3e
WindChen <942979921@qq.com>
2018-02-10 17:28:26 +0800

    a

--- src/main/java/chw/_03_03_3dimensional_model/Client.java
+++ src/main/java/chw/_03_03_3dimensional_model/Client.java
@@ -1,5 +1,8 @@
 package chw._03_03_3dimensional_model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: ${user}
  * @Description:
@@ -8,6 +11,13 @@ package chw._03_03_3dimensional_model;
  */
 public class Client {
     public static void main(String[] args) {
-
+//        BenzModel benz = new BenzModel();
+        BMWModel bmw = new BMWModel();
+        List<String> sequence = new ArrayList<>();
+        sequence.add("engine boom");
+        sequence.add("start");
+        sequence.add("stop");
+        bmw.setSequence(sequence);
+        bmw.run();
     }
 }
